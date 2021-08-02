b80ad0918e22dcc889045c7fef50c26b5e526c39
Brandon Ma <bmn0258@gmail.com>
2021-08-02 14:57:23 -0500

    Updated profile page

--- views/ClientProfile.php
+++ views/ClientProfile.php
@@ -53,7 +53,96 @@
 
             $sql = "SELECT * FROM `clientinformation` WHERE cid='$cid'";
             $result = mysqli_query($conn, $sql);
-            if($result) {
+            $rows = mysqli_num_rows($result);
+            if($rows == 0) {
+          ?>
+          <form action="#" method="post">
+            <div class="request-detail">
+              <div class="input-box">
+                <div class="request">Full Name:</div>
+                <input type="text" name="FullName" value="" maxlength="50" required/>
+              </div>
+              <div class="input-box">
+                <div class="request">Address 1:</div>
+                <input type="text" name="Address" value="" maxlength="100" required>
+              </div>
+              <div class="input-box">
+                <div class="request">Address 2:</div>
+                <input type="text" name="Address2" value="" maxlength="100">
+              </div>
+              <div class="input-box">
+                <div class="request">City:</div>
+                <input type="text" name="City" value="" maxlength="100" required>
+              </div>
+              <div class="input-box">
+                <div class="request">State</div>
+                <select name="State" id="state">
+                  <option value="" selected="selected">Select a State</option>
+                  <option value="AL">Alabama</option>
+                  <option value="AK">Alaska</option>
+                  <option value="AZ">Arizona</option>
+                  <option value="AR">Arkansas</option>
+                  <option value="CA">California</option>
+                  <option value="CO">Colorado</option>
+                  <option value="CT">Connecticut</option>
+                  <option value="DE">Delaware</option>
+                  <option value="DC">District Of Columbia</option>
+                  <option value="FL">Florida</option>
+                  <option value="GA">Georgia</option>
+                  <option value="HI">Hawaii</option>
+                  <option value="ID">Idaho</option>
+                  <option value="IL">Illinois</option>
+                  <option value="IN">Indiana</option>
+                  <option value="IA">Iowa</option>
+                  <option value="KS">Kansas</option>
+                  <option value="KY">Kentucky</option>
+                  <option value="LA">Louisiana</option>
+                  <option value="ME">Maine</option>
+                  <option value="MD">Maryland</option>
+                  <option value="MA">Massachusetts</option>
+                  <option value="MI">Michigan</option>
+                  <option value="MN">Minnesota</option>
+                  <option value="MS">Mississippi</option>
+                  <option value="MO">Missouri</option>
+                  <option value="MT">Montana</option>
+                  <option value="NE">Nebraska</option>
+                  <option value="NV">Nevada</option>
+                  <option value="NH">New Hampshire</option>
+                  <option value="NJ">New Jersey</option>
+                  <option value="NM">New Mexico</option>
+                  <option value="NY">New York</option>
+                  <option value="NC">North Carolina</option>
+                  <option value="ND">North Dakota</option>
+                  <option value="OH">Ohio</option>
+                  <option value="OK">Oklahoma</option>
+                  <option value="OR">Oregon</option>
+                  <option value="PA">Pennsylvania</option>
+                  <option value="RI">Rhode Island</option>
+                  <option value="SC">South Carolina</option>
+                  <option value="SD">South Dakota</option>
+                  <option value="TN">Tennessee</option>
+                  <option value="TX">Texas</option>
+                  <option value="UT">Utah</option>
+                  <option value="VT">Vermont</option>
+                  <option value="VA">Virginia</option>
+                  <option value="WA">Washington</option>
+                  <option value="WV">West Virginia</option>
+                  <option value="WI">Wisconsin</option>
+                  <option value="WY">Wyoming</option>
+                </select>
+              </div>
+              <div class="input-box">
+                <div class="request">Zipcode:</div>
+                <input type="text" name="Zipcode" value="" maxlength="9" minlength="5" required>
+              </div>
+            </div>
+            <div class="form-button">
+              <input type="submit" name="submit" value="Update" />
+            </div>
+          </form>
+          <?php
+            }
+            else {
               while($row = mysqli_fetch_array($result)) {
           ?>
           <form action="#" method="post">
@@ -142,12 +231,7 @@
           </form>
           <?php
               }
-            } 
-            else {
-              echo "Error: " . $sql . "" . mysqli_error($conn);
             }
-            
-            mysqli_close($conn); 
           ?>
         </div>
       </div>
